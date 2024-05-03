249475c526a127987b5e2ac2686007bc9d24267c
eugeniomerloni <164801392+eugeniomerloni@users.noreply.github.com>
2024-05-03 02:58:36 +0000

    photo create works

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ get("/users/:path_username", { :controller => "users", :action => "show"})
 get("/photos", { :controller => "photos", :action => "index"})
 get("/photos/:path_id", { :controller => "photos", :action => "show"})
 get("/delete_photo/:toast_id", { :controller => "photos", :action => "destroy"})
+post("/insert_photo", { :controller => "photos", :action => "create"})
 
 end
